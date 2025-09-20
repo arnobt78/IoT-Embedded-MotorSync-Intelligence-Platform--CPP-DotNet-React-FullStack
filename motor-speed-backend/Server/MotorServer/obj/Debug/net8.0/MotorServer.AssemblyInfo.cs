2b8b2bbe64551503d8fd4e5a95d4b9b115a5f8b8
Arnob Mahmud <arnob_t78@yahoo.com>
2025-09-20 03:15:21 +0200

    update dll and db

--- motor-speed-backend/Server/MotorServer/obj/Debug/net8.0/MotorServer.AssemblyInfo.cs
+++ motor-speed-backend/Server/MotorServer/obj/Debug/net8.0/MotorServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotorServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ee4abf09c56eb1f30b3993c4cc23f220d936ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5125836c6e14f76bd88cf0ccb336be65899d297")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotorServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotorServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
